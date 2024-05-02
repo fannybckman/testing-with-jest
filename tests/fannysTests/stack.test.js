7a6303d9b9c92b6504f79543618eb27a4ac8baa3
fannybckman <fannybeckman01@gmail.com>
2024-05-02 16:14:25 +0200

    lade till fannys fina stack test

---
+++ tests/fannysTests/stack.test.js
@@ -0,0 +1,20 @@
+const stack = require('../src/stack');
+
+test('')
+
+test('strlk på stack ökas när man pushat element', () => {
+    stack.push("wow");
+    stack.push("coolt");
+    expect(stack.size()).toBe(2);
+});
+
+test('strlk på stack minskar när man poppat element', () => {
+    stack.push("eller");
+    stack.push("kanske");
+    stack.pop();
+    expect(stack.size()).toBe(1);
+});
+
+test('strlk på tom stack är 0', () => {
+    expect(stack.size()).toBe(0);
+});
